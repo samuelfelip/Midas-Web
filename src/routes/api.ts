6e5350ead0c825fa3ccffafc36fab41d18762e06
Raiza Mendoza <raiza.mendozaf@gmail.com>
2025-03-14 11:51:53 -0500

    Revert "feat: add shopping cart and course packages functionality"
    This reverts commit fe7328ce9726e2c8de0066cf3fce6e6d44bb6c96.

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,20 +1,11 @@
 import { Router } from 'express';
 import { toolController } from '../controllers/toolController';
 import { isAdmin } from '../middleware/adminAuth';
-import { cartController } from '../controllers/cartController';
-import { packageController } from '../controllers/packageController';
 
 const router = Router();
 
 router.get('/tools', toolController.getTools);
 router.post('/tools', isAdmin, toolController.createTool);
 router.put('/tools/:id', isAdmin, toolController.updateTool);
-
-router.get('/cart', cartController.getCart);
-router.post('/cart/items', cartController.addToCart);
-router.delete('/cart/items/:itemId', cartController.removeFromCart);
-
-router.get('/packages', packageController.getPackages);
-router.post('/packages', isAdmin, packageController.createPackage);
 
 export default router; 
